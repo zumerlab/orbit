85b0910f6eb3ebe99da0e6d27ed7c57d6d40f1b4
Juan Martin <martinmuda@gmail.com>
2024-07-28 14:39:03 +0300

    change names and fix utility bus

--- src/orbit.js
+++ src/orbit.js
@@ -1,10 +1,10 @@
 import { OrbitProgress } from './js/orbit-progress.js'
-import { OrbitSector } from './js/orbit-sector.js'
-import { OrbitLabel } from './js/orbit-label.js'
+import { OrbitSlice } from './js/orbit-slice.js'
+import { OrbitText } from './js/orbit-text.js'
 import { Orbit } from './js/orbit-resize.js'
 
 customElements.define('o-progress', OrbitProgress)
-customElements.define('o-sector', OrbitSector)
-customElements.define('o-label', OrbitLabel)
+customElements.define('o-slice', OrbitSlice)
+customElements.define('o-text', OrbitText)
 
 window.Orbit = Orbit
