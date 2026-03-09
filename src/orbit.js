3889aa5abe450ab48d14659c6e9bddeed560217e
Juan Martin <martinmuda@gmail.com>
2026-03-08 22:26:47 -0400

    Refactor custom element registration in orbit.js for improved readability

--- src/orbit.js
+++ src/orbit.js
@@ -2,12 +2,6 @@ import { OrbitProgress } from './js/orbit-progress.js'
 import { OrbitArc } from './js/orbit-arc.js'
 import { Orbit } from './js/orbit-resize.js'
 
-// To prevent re-registration attemps in some environments such as CodeSandBox
-if (!customElements.get('o-progress')) {
-    customElements.define("o-progress", OrbitProgress);
-  }
-if (!customElements.get('o-arc')) {
-    customElements.define("o-arc", OrbitArc);
-}
-
+if (!customElements.get('o-progress')) customElements.define('o-progress', OrbitProgress)
+if (!customElements.get('o-arc')) customElements.define('o-arc', OrbitArc)
 window.Orbit = Orbit
